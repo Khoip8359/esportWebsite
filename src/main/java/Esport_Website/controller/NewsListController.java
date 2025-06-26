dfd8117d645ae0519ced10715dcec368379c2201
Ho Tuan Canh <canh123@gmail.com>
2025-06-26 16:48:36 +0700

    Đẩy code lên nhánh canh

--- src/main/java/Esport_Website/controller/NewsListController.java
+++ src/main/java/Esport_Website/controller/NewsListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,5 +51,15 @@ public class NewsListController {
 		List<News> hotNews = newsService.getSuggestNews();
 		
 		return hotNews;
+	}
+
+	@GetMapping("/api/news/search")
+	public Page<News> searchNews(
+	    @RequestParam String keyword,
+	    @RequestParam(defaultValue = "0") int page,
+	    @RequestParam(defaultValue = "5") int size) {
+	    
+	    PageRequest pageable = PageRequest.of(page, size);
+	    return newsService.searchNews(keyword, pageable);
 	}
 }
