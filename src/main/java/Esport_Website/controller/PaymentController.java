53f820ecdd153f764a67854b873f2e2cf36d31ef
Khoip8359 <phamkhoi9988@gmail.com>
2025-07-21 07:36:43 +0700

    Add data pool

---
+++ src/main/java/Esport_Website/controller/PaymentController.java
@@ -0,0 +1,8 @@
+package Esport_Website.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+	
+}
--- src/main/java/Esport_Website/utils/MakeMeWake.java
+++ src/main/java/Esport_Website/utils/MakeMeWake.java
@@ -1,5 +1,6 @@
 package Esport_Website.utils;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,8 @@ public class MakeMeWake {
 	
 	private final RestTemplate restTemplate = new RestTemplate();
 	
-	@Scheduled(fixedRate = 1000 * 60 * 10)
+//	@Scheduled(fixedRate = 1000 * 60 * 10)
+	@Async
     public void pingMyself() {
         try {
             String url = "https://ganews.onrender.com/ping";
